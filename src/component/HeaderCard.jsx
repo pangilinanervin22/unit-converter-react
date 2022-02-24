1025492c01be694a8f5f7c16b200fef57e76932f
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-02-24 20:51:59 +0800

    more options added

--- src/component/HeaderCard.jsx
+++ src/component/HeaderCard.jsx
@@ -5,7 +5,15 @@ import Select from "../common/Select";
 const options = [
 	{ value: "length", label: "length" },
 	{ value: "mass", label: "mass" },
+	{ value: "area", label: "area" },
+	{ value: "temperature", label: "temperature" },
+	{ value: "volume", label: "volume" },
 	{ value: "speed", label: "speed" },
+	{ value: "time", label: "time" },
+	{ value: "frequency", label: "frequency" },
+	{ value: "voltage", label: "voltage" },
+	{ value: "force", label: "force" },
+	{ value: "pressure", label: "pressure" },
 ];
 
 function HeaderCard({ onChange, value }) {
