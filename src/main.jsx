40de361c8ab1392cf2e36d6fd86bcb6107acb4e1
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-11-12 00:24:15 +0800

    update dep

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./scss/main.scss";
 
 import App from "./app";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
