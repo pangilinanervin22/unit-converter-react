f5f9ec20226be7251e0bee81bdaa37c2955c172d
Ervin <pangilinanervin22@gmail.com>
2023-03-31 14:00:51 +0800

    test pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/unit-converter-react/",
-	plugins: [react({ jsxRuntime: "classic" })],
+  base: "/unit-converter-react/",
+  plugins: [
+    react(
+      { jsxRuntime: "classic" },
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+        icons: [
+          {
+            src: "react.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "react.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "react.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      })
+    ),
+  ],
 });
